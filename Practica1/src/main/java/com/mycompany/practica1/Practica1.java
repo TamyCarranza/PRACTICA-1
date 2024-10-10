7cff056f29e32528c02d7a315862e5ca1ae3e043
TamyCarranza <157515215+TamyCarranza@users.noreply.github.com>
2024-10-09 19:05:08 -0600

    Add files via upload
    Practica 1 Evaluada

---
+++ Practica1/src/main/java/com/mycompany/practica1/Practica1.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tamar
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+           // Solicitar la cantidad de empleados
+
+           int cantEmpleados = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de empleados:"));
+           
+           //Variable SEM Y IVT
+        double totalSEM = 0;
+        double totalIVM = 0;
+
+        // Solicitar el salario por empleado
+        for (int i = 1; i <= cantEmpleados; i++) {
+            double salario = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el salario del empleado #" + i + ":"));
+
+            // Calcular SEM e IVM
+            double SEM = salario * 0.0925;
+            double IVM = salario * 0.0508;
+
+            totalSEM += SEM;
+            totalIVM += IVM;
+        }
+        
+        // Calcular total de la caja
+        double totalPagar = totalSEM + totalIVM;
+
+        // Mensaje final
+        String mensajeFinal = String.format("Su empresa deberá abonar a la CCSS un monto de " + totalPagar + " colones por concepto de SEM y IVM.");
+        JOptionPane.showMessageDialog(null, mensajeFinal);
+            }
+        }     
+        
+        
+        
+        
+        
+        
+    
